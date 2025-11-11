5179ead3b73e997a26408b87912bcf0946733efa
Arturoooooo <35744158+Arturchel174@users.noreply.github.com>
2025-11-12 01:12:20 +0500

    init basic yii2

---
+++ www/site/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=site_mysql;dbname=site',
+    'username' => 'root',
+    'password' => 'site',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
